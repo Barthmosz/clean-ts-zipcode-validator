59c404dbceb6f446269dedfdb51233aa67a1badd
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-06 12:14:33 -0300

    docs: add jsdocs to ZipCodeValidator

--- src/zipcode-validator.ts
+++ src/zipcode-validator.ts
@@ -1,4 +1,9 @@
 export class ZipCodeValidator {
+    /**
+     * Validates a zip code.
+     * @param {string} zipCode
+     * @returns {boolean}
+    */
     static validate(zipCode: string): boolean {
         const zipCodeRegex = /^\d{5}$/
         return zipCodeRegex.test(zipCode)
